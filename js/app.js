beaa07500a385c362906dd5abf3886cd3049e3c5
dulockwood <dul@live.unc.edu>
2014-12-07 15:44:07 -0500

    Alpha Push
    While far from complete, I need to get SOMETHING up for the final
    project turn-in

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
-
-//place functions for data connect here...
+//Hey, Steven.  Don't know if you're gonna read this, but I want to say that I hate that I wasn't able to get further with this project this semester.
+//I'd love to keep going with it until it's what I had originally planned it to be.
 
 $(document).foundation();
 
-function loadTasksActive(){
+function loadTasksActive(){ //-------------------------------------------MAIN TASKS POPULATED HERE--------------------------------//
       if ($('body').is('.index')){
      // Create our Firebase reference
-     var tasksToDisplay = 7;
+     var tasksToDisplay = 7;  //not going to do anything without the ".limit(tasksToDisplay);" not being commented out
      var taskListRef = new Firebase('https://designr.firebaseio.com//tasks');
-     var taskListView = taskListRef.limit(tasksToDisplay);
+     var taskListView = taskListRef.length;  //.limit(tasksToDisplay);
 
      taskListRef.once('value', function(dataSnapshot) { //getting a value immediately at URL then outputing a Snapshot
             var listItems = "";
@@ -25,10 +25,12 @@ function loadTasksActive(){
                     
                     $.each(val, function(key, val) {
                                     
-                                        var taskStatus = task[6];
-                                        console.log(taskStatus);
-                                             task.push(val);
-                    });
+                                        //var taskStatus = task[6];
+                                       // console.log(taskStatus);
+                                        console.log(val);
+                                        console.log(key);
+                                        task.push(val);
+                   } );
                     
                     listItems += '<dd class="accordion-navigation accordion-topEntry taskClass"><a href="#link_' + task[0] + '">'
                     listItems += '<div class="accordion-titlebar-heightFix" id="' + task[0] + '"><div class="date-div right"><p class="accordion-titleFix date-p">' + task[1] +'</p></div>'
@@ -37,28 +39,28 @@ function loadTasksActive(){
                     listItems += '<p class="description-p">' + task[7] + '</p></div></div></a>'
                     listItems += '<div id="link_' + task[0] + '" class="content"><div class="row"><div class="large-12 column squeeze"><div class="right">'
                     listItems += '<p class="textRight time-text">' + task[4] + ' hours projected</p></div>'
-                    listItems += '<div class="left timerToggle" style="margin-top: -3px"><img src="img/icon-timerOff-24x24.png" alt="icon-timerOff-24x24"></div>' //------Need to remove inline style at some point
-                    listItems += '<div style="margin-left: 8px" class="left time-text" id="countdown_' + task[0] +'"></div></div><div class="large-12 column squeeze">'
-                    listItems += '<p class="description-p">' + task [2] + '</p><div class="right" style="margin-top: 5px"><img src="img/icon-cancel-24x24.png" alt="icon-cancel-24x24"></div>'
-		    listItems += '<div class="left" style="margin-top: 5px"><img src="img/icon-chevronLeft-big-24x24.png" alt="icon-chevronLeft-24x24"></div></div></div></div></dd>'
+                    listItems += '<div class="left" style="margin-top: -3px"><img src="img/icon-timerOff-24x24.png" alt="icon-timerOff-24x24" class="' + task[0] + '" id="img_' + task[0] + '"></div>' //------Need to remove inline style at some point
+                    listItems += '<div style="margin-left: 8px" class="left time-text" id="countdown_' + task[0] +'"></div></div><div class="large-12 column squeeze">' //-----point in html where .countdown manifests
+                    listItems += '<p class="description-p">' + task [2] + '</p><div class="right" style="margin-top: 5px"><img src="img/icon-cancel-24x24.png" alt="icon-cancel-24x24" class="' + task[0] + '"></div>' // -----targetting the img src for the cancel functioon
+		    listItems += '<div class="left" style="margin-top: 5px"><!--<img src="img/icon-chevronLeft-big-24x24.png" alt="icon-chevronLeft-24x24">--></div></div></div></div></dd>' //HTML INCLUDES COMMENTING TO GET RID OF HISTORY ARROW.
                     
             });
-            console.log("accordian finished");
+            console.log("accordian finished");  //-----testing hierarchy of functions; code does not run properly unless the html is built first
             
             $("#task-list").html(listItems);
             
-            setUp();
+            setUp(); //-----initializing 
      }); //end taskListRef.once();
       }
 }  // end loadTasksActive();
 
-function loadTasksHistory(){
+function loadTasksHistory(){ //--------------------------------HISTORY TASKS POPULATED HERE----------------------------------//
      if ($('body').is('.history')){
 
      // Create our Firebase reference
      var tasksToDisplay = 7;
      var taskListRef = new Firebase('https://designr.firebaseio.com//tasks');
-     var taskListView = taskListRef.limit(tasksToDisplay);
+     var taskListView = taskListRef.legnth; //limit(tasksToDisplay);
 
      taskListRef.once('value', function(dataSnapshot) { //getting a value immediately at URL then outputing a Snapshot
             var listItems = "";
@@ -82,7 +84,7 @@ function loadTasksHistory(){
                     
                     listItems += '<dd class="accordion-navigation accordion-topEntry taskClass"><a href="#link_' + task[0] + '">'
                     listItems += '<div class="accordion-titlebar-heightFix" id="' + task[0] + '"><div class="date-div right"><p class="accordion-titleFix date-p">' + task[1] +'</p></div>'
-                    listItems += '<div class="left circle"></div>'
+                    //listItems += '<div class="left circle"></div>'
                     listItems += '<div class="left"><p class="darker-p">' + task[5] + '</p>'
                     listItems += '<p class="description-p">' + task[7] + '</p></div></div></a>'
                     listItems += '<div id="link_' + task[0] + '" class="content"><div class="row"><div class="large-12 column squeeze"><div class="right">'
@@ -104,39 +106,107 @@ function loadTasksHistory(){
 var DATA_LOCATION = 'https://designr.firebaseio.com//tasks';
 
 function setUp(){
-      if ($('body').is('.index')){
-          console.log("setUp running");
+      if ($('body').is('.index')){  //-----the counting function should only apply for the main page.
+          console.log("setUp running"); //-----testing the hierarchy of the functions.  Both of the loadTasks(); should run before setUp();
           $('.taskClass').click((function(){
-     
-                       var itemClicked = event.target.id;
-                       console.log(itemClicked);
-                       var startDate = new Date();
-                       var currentAccordion =  "countdown_" + itemClicked;
+                         
+                       var itemClicked = event.target.id;  //-----pulls the ID of the main accordion <div>
+                       console.log(itemClicked);  //-----checking ID
+                       var startDate = new Date();  //-----starts counter at 00:00:00; pulls current time in milliseconds since 1970
+                       var firebaseDate = ""; //-----------COME BACK TO THIS RIGHT HERE
+                       var currentAccordion =  "countdown_" + itemClicked;  //-----distinguishing ID but incorporating the same unique string
                        $("#" + currentAccordion).countdown({since: startDate, compact: true, 
-                       format: 'HMS', description: ''});
-                       console.log(itemClicked);
-                       checkIfUserExists(itemClicked);
-                       console.log(currentAccordion);
+                       format: 'HMS', description: ''}); //-----.countdown is defined in jquery.countdown.css
+                       //checkIfUserExists(itemClicked);
+                       console.log(currentAccordion); //----making sure we're getting the right result out of all this
                })//enclosing anonymous function of $('.taskClass')
                
                     
 	  ); //end $('.taskClass')
-      }
-                                          
-          function checkIfUserExists(itemClicked) { //Searching the database at the given location and spitting out a value of "hoursActive"
-                  var termRef = new Firebase(DATA_LOCATION);
-                  termRef.child(itemClicked).once('value', function(snapshot) {
-                  var singleTask = snapshot.val();
-                  console.log(singleTask.hoursActive);
+          $("img[src='img/icon-cancel-24x24.png']").click((function(){
+     
+                       var itemClicked = $(event.target).attr('class');
+                       console.log(itemClicked + " cancel button");
+                       checkIfUserExists(itemClicked);
+                       var currentRef = new Firebase('https://designr.firebaseio.com//tasks//' + itemClicked);
+                       currentRef.remove();
+                         
+                       location.reload();
+                       console.log(currentRef);
+               })//enclosing anonymous function of $('img[src=icon cancel]')
+               
+               
+	  ); //end $('img[src=icon cancel]')
           
-          });
-          }
+           $("img[src='img/icon-timerOff-24x24.png']").click((function(){ //-----I'd love to switch out the icon with another one on each toggle, but I don't know how to write a loop for that.
+                       var itemClicked = $(event.target).attr('class');  //------I used class identifiers in conjunction with the unique ID to make things easier on myself down the line with functions.
+                       console.log(itemClicked + " cancel button");  //-----Because having affirmation in the Console is nice.
+                      $('#countdown_' + itemClicked + '').countdown('toggle'); //-----.countdown('toggle') is a jquery.countdown.js-defined function.
+                      var countData = $("#countdown_" + itemClicked).countdown('getTimes');
+                      var text = '';
+                      for (var i = 0; i < countData.length; i++) {
+                         text += countData[i] + '' + $.countdown.regionalOptions[''].labels[i] + ' ';
+                      }
+                      console.log(countData);
+                      console.log(text);
+ //                     var countFinal //-----This variable will hold the computations necessary to convert the string countData into milliseconds to be saved on the server.
+               })//enclosing anonymous function of $('img[src=icon timerOff]')
+                    
+	  ); //end $('img[src=icon timerOff]')
+                     
+                    }
+                                          
+         // function checkIfUserExists(itemClicked) { //Searching the database at the given location and spitting out a value of "hoursActive"
+         //         var termRef = new Firebase(DATA_LOCATION);
+          //        termRef.child(itemClicked).once('value', function(snapshot) {
+          //        var singleTask = snapshot.val();
+         //         console.log(singleTask.hoursActive);
+          
+         // });
+          //}
           
        //----------end if statement
                                           
+                                          
+
  } // --------- end setUp();
 
+//$(window).unload(function() { //----------To update thevalues of hours active for each task upon reloading COMMENTING OUT FOR FUNCTIONALITY'S SAKE
+ //         var taskListRef = new Firebase('https://designr.firebaseio.com//tasks'); //-----everything at this line and below was copied from elsewhere in hopes of changing specifically one object based on the time at .unload();
+ //         var taskListView = taskListRef.length;  //.limit(tasksToDisplay);
 
+//          taskListRef.once('value', function(dataSnapshot) { //getting a value immediately at URL then outputing a Snapshot
+            // store dataSnapshot for use in below examples.
+//           taskListView = dataSnapshot.val();
+            
+ //         $.each(taskListView, function(key, val) { //-----Copying pasting-to-html structure; "key" = unique ID. "val" = strings under that val
+ //                var task = [];
+                    
+ //                task.push(key);  //-----not sure if this is necessary for executing this part
+                    
+  //                $.each(val, function(key, val) { //key refers to object name, val refers to object value; different thank tier above
+//                                    if (key ==="hoursActive") { //only at this level of the function is "key" describing the name of the object/data, rather than the Unique ID
+  //                                      $.set() //trying to set the value of hoursActive to the countData.  But how do I do that with this being out of scope?
+  //                                  } else { //don't think an "else" is actually required.
+ //                                       console.log("This is crap");
+//                                       var taskStatus = task[3];
+//                                       console.log(taskStatus); }
+                                          // task.push(val);
+ //                  });
+//    for (test) {
+          //loop through each of the variables from the data location. Get each time and append the value on the server
+//     }
+//     var countData = $("#countdown_" + itemClicked).countdown('getTimes');
+//                      var text = '';
+ //                     for (var i = 0; i < countData.length; i++) {
+ //                        text += countData[i] + '' + $.countdown.regionalOptions[''].labels[i] + ' ';
+  //                    }
+ //                     console.log(countData);
+ //                     console.log(text);
+//});
+          
+ //         });
+//});//----------end $(window).unload(function(){}); -------------------AND END OF COMMENTING-OUT CHAIN
 
 var ref = new Firebase('https://designr.firebaseio.com//tasks');
 
@@ -167,16 +237,6 @@ var ref = new Firebase('https://designr.firebaseio.com//tasks');
     
 };
 
-$('.task').click(function(){
-          
-          var dateFunction = new Date();
-          var clickTime = dateFunction.getTime();
-          
-          
-          
-     
-});
-
 $('#submit').click(function() {
     location.reload();
 });
@@ -194,22 +254,6 @@ $('#pageTransfer').click(function(){
      
 //});
 
-//var searchTerm = prompt('Task name?', '-JZu_PngKDVu-VuJDbRm');
-
-//function go() {
-          
- //         checkIfUserExists(searchTerm);
-//}
-
-
-
-//function termExistsCallback(searchTerm, exists) {
-   //       if (exists) {
- //         alert('user ' + searchTerm +  ' exists!');
-   //       } else {
-   //       alert('user ' + searchTerm + ' does not exist!');
- //}
-//}
 
 //function checkIfUserExists(searchTerm) {
 //          var termRef = new Firebase(DATA_LOCATION);
@@ -221,15 +265,9 @@ $('#pageTransfer').click(function(){
 //});
 //}
 
-// Tests to see if /users/<searchTerm> has any data.
 
 
 
-//Set up Function
-//function updateTask(){
-    //var value = prompt('Change the hoursEstimate to:', '21');
-   //console.log(ref);
-    //var taskRef = ref.child("Logo Redesign");
     
     //taskRef.update({
         //"hoursEstimate" : value
@@ -237,20 +275,6 @@ $('#pageTransfer').click(function(){
    //console.log("done running updateTask()")
 
 //}
-
-//function postTask(){
-  //  ref.push("App Project");
-   // var taskRef = ref.child("App Project");
-    //ref.push({
-      //  name: "Whatevs",
-      //  date: "20140401",
-        //description: "this shits gone meta",
-       // hoursActive: "0",
-        //hoursEstimate: "9001",
-       // state: "active"
-    //});
-//}
-
 
 function onDeviceReady() {
     //needed only for mobile deployment
